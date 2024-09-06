07b175ec4025b716fbdc1ab1d841c72681b552da
RockinChaos <JacobGaming@CraftationGaming.com>
2024-09-05 21:05:17 -0700

    chore: better data fetching

--- src/mixins/external-data.js
+++ src/mixins/external-data.js
@@ -4,35 +4,25 @@ import axios from "axios";
 const pluginsDefaults = {
     itemjoin: {
         name: "ItemJoin",
-        description: "Get customs item on join",
-        supportedVersions: "1.20.4, 1.19.4, 1.18.2, 1.17.1, 1.16.5, 1.15.2, 1.15.2, 1.14.4, 1.13.2",
-        deprecatedVersions: "1.8.8, 1.9.4, 1.10.2, 1.11.2, 1.12.2",
-        releaseVersions: "1.20.2, 1.19.4, 1.18.2, 1.17.1, 1.16.5, 1.15.2, 1.15.2, 1.14.4, 1.13.2",
+        description: "Get custom items on join",
         downloadUrl: null,
-        gitAPIUrl: "https://api.github.com/repos/RockinChaos/ItemJoin",
-        premiumUrl: null,
-        changelogDevUrl: "https://github.com/RockinChaos/ItemJoin/commits/master",
-        changelogReleaseUrl: "https://github.com/RockinChaos/ItemJoin/releases",
+        git: "RockinChaos/ItemJoin",
         spigetUrl: "https://api.spiget.org/v2/resources/12661",
-        commit: null,
+        commits: null,
+        lastCommits: null,
         build: null,
         version: null,
-        premium: false,
         tags: [{ text: 'PLUGIN', color: 'info' }, { text: 'MINECRAFT', color: 'light' }]
     },
     fakecreative: {
         name: "FakeCreative",
         description: "A creative mode emulation",
-        supportedVersions: "1.20.4, 1.19.4, 1.18.2, 1.17.1, 1.16.5, 1.15.2, 1.15.2, 1.14.4, 1.13.2",
-        deprecatedVersions: "1.8.8, 1.9.4, 1.10.2, 1.11.2, 1.12.2",
-        releaseVersions: "1.18.2, 1.17.1, 1.16.5, 1.15.2, 1.15.2, 1.14.4, 1.13.2",
         downloadUrl: null,
-        gitAPIUrl: "https://api.github.com/repos/RockinChaos/FakeCreative",
+        git: "RockinChaos/FakeCreative",
         premiumUrl: "https://www.spigotmc.org/resources/fakecreative.95959/",
-        changelogDevUrl: "https://github.com/RockinChaos/FakeCreative/commits/master",
-        changelogReleaseUrl: "https://www.spigotmc.org/resources/fakecreative.95959/updates",
         spigetUrl: "https://api.spiget.org/v2/resources/95959",
-        commit: null,
+        commits: null,
+        lastCommits: null,
         build: null,
         version: null,
         premium: true,
@@ -41,19 +31,12 @@ const pluginsDefaults = {
     chaoscore: {
         name: "ChaosCore",
         description: "A shaded core utility",
-        supportedVersions: "1.20.4, 1.19.4, 1.18.2, 1.17.1, 1.16.5, 1.15.2, 1.15.2, 1.14.4, 1.13.2",
-        deprecatedVersions: "1.8.8, 1.9.4, 1.10.2, 1.11.2, 1.12.2",
-        releaseVersions: "1.20.2, 1.19.4, 1.18.2, 1.17.1, 1.16.5, 1.15.2, 1.15.2, 1.14.4, 1.13.2",
         downloadUrl: null,
-        gitAPIUrl: "https://api.github.com/repos/RockinChaos/ChaosCore",
-        premiumUrl: null,
-        changelogDevUrl: "https://github.com/RockinChaos/ChaosCore/commits/master",
-        changelogReleaseUrl: "https://github.com/RockinChaos/ChaosCore/releases",
-        spigetUrl: null,
-        commit: null,
+        git: "RockinChaos/ChaosCore",
+        commits: null,
+        lastCommits: null,
         build: null,
         version: null,
-        premium: false,
         tags: [{ text: 'LIBRARY', color: 'warning' }, { text: 'MINECRAFT', color: 'light' }]
     }
 };
@@ -100,6 +83,7 @@ export default {
     },
     methods: {
         refreshDownloads() {
+            getDescription();
             getJenkins();
             getLatestRelease();
         }
@@ -114,7 +98,7 @@ async function getDownloads() {
         if (url != null) {
             try {
                 response = await axios.get(url);
-                count += response.data.downloads * 2; // (* 2 temporary) for Bukkit/CurseForge download count.
+                count += response.data.downloads * 2.8; // (* 2.8 temporary) for Bukkit/CurseForge download count.
             } catch (e) {
                 count += state.downloads;
             }
@@ -149,7 +133,7 @@ async function getStars() {
 
     for (const name in pluginsDefaults) {
         try {
-            let response = await axios.get(state.builds.stable.plugins[name].gitAPIUrl);
+            let response = await axios.get('https://api.github.com/repos/' + state.builds.stable.plugins[name].git);
             stars += response.data.stargazers_count;
             forks += response.data.forks_count;
         } catch (e) { }
@@ -159,55 +143,107 @@ async function getStars() {
     state.github.forks = forks;
 }
 
-const mainCI = "https://jenkins.craftationgaming.com/job/job_id/";
-const versionRegex = "job_id[a-zA-Z]*-([0-9.]+?(?:-(?:SNAPSHOT|BETA|ALPHA|EXPERIMENTAL)-[a-zA-Z][0-9]+)?(?:-([0-9a-fA-F]+?))?).jar";
-
 function getVersionFromArtifact(name, fileName) {
-    let m;
-    while ((m = new RegExp(versionRegex.replace("job_id", name)).exec(fileName)) !== null) {
-        return m[1];
+    const match = new RegExp(versionRegex.replace("job_id", name)).exec(fileName);
+    return match ? 'v' + match[1] : null;
+}
+
+function parseCommitMessage(commitId, comment) {
+    const lines = comment.split('\u000a');
+    const firstLine = lines[0];
+    const typeMatch = firstLine.match(/^(fix|feat|chg|chore|bump|ign):/);
+    const commitType = typeMatch ? typeMatch[1] + ':' : 'other:';
+    const commitName = typeMatch ? firstLine.split(':')[1].trim() : firstLine.trim();
+    const commitDescription = lines.slice(1).join(' ').trim();
+    return (commitName.match(/-RELEASE|-SNAPSHOT/) || commitType.match(/^(bump|ign):/))
+        ? null
+        : { commitId, commitType: commitType.charAt(0).toUpperCase() + commitType.slice(1), commitName, commitDescription };
+}
+
+async function getCommits(currentCI, name, buildNumber, lastResponse) {
+    let releaseFound = state.builds.stable.plugins[name].premium
+        ? lastResponse.data.changeSet.items.some(item => item.comment.includes('-RELEASE'))
+        : lastResponse.data.artifacts.some(artifact => artifact.fileName.includes('RELEASE'));
+    lastResponse.data.changeSet.items.forEach(item => {
+        if (item.commitId) {
+            const commits = parseCommitMessage(item.commitId, item.comment);
+            if (commits) {
+                state.builds.dev.plugins[name].lastCommits.push(commits);
+                const stateCommits = releaseFound
+                    ? state.builds.stable.plugins[name].commits
+                    : state.builds.dev.plugins[name].commits;
+                stateCommits.push(commits);
+            }
+        }
+    });
+    let stop = false;
+    while (!stop && buildNumber) {
+        try {
+            let response = await axios.get(`${currentCI}${buildNumber}/api/json`);
+            const isReleaseArtifact = state.builds.stable.plugins[name].premium
+                ? response.data.changeSet.items.some(item => item.comment.includes('-RELEASE'))
+                : response.data.artifacts.some(artifact => artifact.fileName.includes('RELEASE'));
+            if (isReleaseArtifact) {
+                releaseFound ? stop = true : releaseFound = true;
+            }
+            if (!stop) {
+                const stateCommits = releaseFound ? state.builds.stable.plugins[name].commits : state.builds.dev.plugins[name].commits;
+                response.data.changeSet.items.forEach(item => {
+                    if (item.commitId) {
+                        const commits = parseCommitMessage(item.commitId, item.comment);
+                        if (commits) {
+                            stateCommits.push(commits);
+                        }
+                    }
+                });
+            }
+            buildNumber = response.data.id - 1;
+        } catch (e) {
+            if (e.response && e.response.status === 404) {
+                buildNumber -= 1;
+            } else {
+                break;
+            }
+        }
     }
 }
 
-function getCommitIdFromArtifact(name, fileName) {
-    let m;
-    while ((m = new RegExp(versionRegex.replace("job_id", name)).exec(fileName)) !== null) {
-        return m[2];
-    }
-}
+const mainCI = "https://ci-dev.craftationgaming.com/job/job_id/";
+const versionRegex = "job_id[a-zA-Z]*-([0-9.]+?(?:-(?:RELEASE|SNAPSHOT|BETA|ALPHA|EXPERIMENTAL)-[a-zA-Z][0-9]+)?(?:-([0-9a-fA-F]+?))?).jar";
 
 async function getJenkins() {
     try {
         state.builds.dev.loading = true;
+        state.builds.stable.loading = true;
+
         for (const name in pluginsDefaults) {
             let currentCI = mainCI.replace("job_id", name);
-            let response = await axios.get(`${currentCI}lastSuccessfulBuild/api/json`);
+            state.builds.dev.plugins[name].commits = [];
+            state.builds.dev.plugins[name].lastCommits = [];
+            state.builds.stable.plugins[name].commits = [];
+
+            let response = name.includes("itemjoin") ? await axios.get(`${currentCI}1030/api/json`) : await axios.get(`${currentCI}lastSuccessfulBuild/api/json`);
             state.builds.dev.plugins[name].build = response.data.id;
             state.builds.dev.plugins[name].version = getVersionFromArtifact(state.builds.dev.plugins[name].name, response.data.artifacts[0].displayPath);
-            if (response.data.changeSet.items[0]) {
-                state.builds.dev.plugins[name].commit = response.data.changeSet.items[0].commitId;
-            } else {
-                state.builds.dev.plugins[name].commit = getCommitIdFromArtifact(state.builds.dev.plugins[name].name, response.data.artifacts[0].displayPath);
-            }
-            if (!state.builds.dev.plugins[name].commit) {
-                state.builds.dev.plugins[name].commit = null;
-            }
+            state.builds.dev.plugins[name].downloadUrl = `${currentCI}lastSuccessfulBuild/artifact/${response.data.artifacts[0].relativePath}`;
+            getCommits(currentCI, name, (response.data.id - 1), response);
             state.builds.dev.error = null;
-            response.data.artifacts.forEach(artifact => {
-                state.builds.dev.plugins[name].downloadUrl = `${currentCI}lastSuccessfulBuild/artifact/${artifact.relativePath}`;
-            });
+            state.builds.stable.error = null;
         }
     } catch (e) {
         state.builds.dev.error = e.response ? e.response.data : e.message;
+        state.builds.stable.error = e.response ? e.response.data : e.message;
+    } finally {
+        state.builds.dev.loading = false;
+        state.builds.stable.loading = false;
     }
-    state.builds.dev.loading = false;
 }
 
 async function getLatestRelease() {
     try {
         state.builds.stable.loading = true;
         for (const name in pluginsDefaults) {
-            const { data } = await axios.get('https://api.github.com/repos/RockinChaos/' + name + '/releases');
+            const { data } = await axios.get('https://api.github.com/repos/' + state.builds.stable.plugins[name].git + '/releases');
             state.builds.stable.plugins[name].version = data[0].tag_name + "-RELEASE";
             state.builds.stable.error = null;
             data[0].assets.forEach(asset => {
@@ -220,6 +256,21 @@ async function getLatestRelease() {
     state.builds.stable.loading = false;
 }
 
+async function getDescription() {
+    for (const name in pluginsDefaults) {
+        let currentCI = mainCI.replace("job_id", name);
+
+        let response = await axios.get(`${currentCI}api/json`);
+        let description = response.data.description.split('ci-import">')[1].split('<div class="ci-no-import"')[0];
+        let devParts = description.split('<ul class="release-versions"')
+        state.builds.dev.plugins[name].description = devParts[0] + '<ul class="snapshot-versions"' + devParts[1].split('<ul class="legacy-versions"')[1];
+
+        let releaseParts = description.split('<ul class="snapshot-versions"')
+        state.builds.stable.plugins[name].description = releaseParts[0] + '<ul class="release-versions"' + releaseParts[1].split('<ul class="release-versions"')[1];
+    }
+}
+
+getDescription();
 getJenkins();
 getLatestRelease();
 getMembers();
